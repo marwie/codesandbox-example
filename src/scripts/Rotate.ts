3bc7c343e7924e30fc85687fb2a3ea6a026ed9e1
Marcel Wiessler <marwie@users.noreply.github.com>
2023-04-28 16:53:05 +0000

    Update 5 files

--- src/scripts/Rotate.ts
+++ src/scripts/Rotate.ts
@@ -1,7 +1,10 @@
-import { Behaviour } from "@needle-tools/engine";
+import { Behaviour, OpenURL, showBalloonMessage } from "@needle-tools/engine";
 
 export class Rotate extends Behaviour {
-    update(): void {
-        this.gameObject.rotateY(this.context.time.deltaTime);
-    }
+  update(): void {
+    this.gameObject.rotateY(this.context.time.deltaTime * 0.5);
+
+    this.gameObject.position.y +=
+      Math.sin(this.context.time.time) * this.context.time.deltaTime;
+  }
 }
